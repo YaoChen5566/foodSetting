b62564b4e15b976adc1442109e92a029bbf570e3
YaoChen5566 <dick02040204@gmail.com>
2016-10-18 20:24:06 +0800

    20161018edit
    propotion of contour and evaluate speed of getting score

--- descri.cpp
+++ descri.cpp
@@ -127,7 +127,7 @@ float descri::contourLength(vector<Point> singleContour)
 // return vector of sample point
 vector<Point> descri::getSamplePoints(vector<Point> singleContour)
 {
-	int pointCount = singleContour.size()/2;
+	int pointCount = singleContour.size()/4;
 	int totalPoints = singleContour.size();
 
 	vector<int> pointIndex;
