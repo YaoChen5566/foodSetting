9d392e20cbc02190e70bd42f3005688b12b6ac88
YaoChen5566 <dick02040204@gmail.com>
2016-10-24 18:56:57 +0800

    20161024 final edit
    change compare into class
    clean the code

---
+++ compare.h
@@ -0,0 +1,34 @@
+#include <opencv\cv.h>
+#include <opencv2\highgui\highgui.hpp>
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+# define PI 3.1415926
+
+using namespace std;
+using namespace cv;
+
+class comp
+{
+public:
+	
+	int startIndex1;
+	int startIndex2;
+	int range;
+	int score;
+
+	comp(Mat des1, Mat des2);
+
+	
+	
+private:
+	
+	void compareDes(Mat input1, Mat input2);
+	Mat subMatrix(Mat input, int row, int col, int range);
+
+};
